98ebffa73a1453356becc78c8428a36c5acec56d
Icram Doku <icramdoku@gmail.com>
2023-10-03 21:55:23 -0400

    add new code

---
+++ assignment1-app/backend/README.md
@@ -0,0 +1,14 @@
+"""THIS Connection is valid:
+->Run Python File
+The schema does not need to be setup, the program will create it if it does not exist already
+defining the database is important tho
+Tested with:
+C:\Users\icram>curl --X POST -H "Content-Type: application/json" --data-binary "{\"_id\": \"cat\", \"username\": \"john_doe\", \"name\": \"John Doe\", \"password\": \"password123\"}" http://127.0.0.1:5000/users
+curl: (6) Could not resolve host: POST
+{
+  "message": "User created successfully"
+}
+
+C:\Users\icram>curl http://127.0.0.1:5000/users
+[]
+"""
--- assignment1-app/backend/app.py
+++ assignment1-app/backend/app.py
@@ -1,20 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson import ObjectId  # Import ObjectId from bson module
-"""THIS Connection is valid:
-->Run Python File
-The schema does not need to be setup, the program will create it if it does not exist already
-defining the database is important tho
-Tested with:
-C:\Users\icram>curl --X POST -H "Content-Type: application/json" --data-binary "{\"_id\": \"cat\", \"username\": \"john_doe\", \"name\": \"John Doe\", \"password\": \"password123\"}" http://127.0.0.1:5000/users
-curl: (6) Could not resolve host: POST
-{
-  "message": "User created successfully"
-}
 
-C:\Users\icram>curl http://127.0.0.1:5000/users
-[]
-"""
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://mongo:BhYSvmZJmN1ERVgO@cmsc447.ymkauhy.mongodb.net/test'
