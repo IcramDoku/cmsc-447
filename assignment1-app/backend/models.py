a3c30de2f743bae4e9de1dcd63caf26a5add4a71
Icram Doku <icramdoku@gmail.com>
2023-10-04 21:47:06 -0400

    update models for database

--- assignment1-app/backend/models.py
+++ assignment1-app/backend/models.py
@@ -1,6 +1,6 @@
 from flask_pymongo import ObjectId
 
-class User:
+class Users:
     def __init__( self, username, name, password):
         self.username = username
         self.name = name
@@ -15,13 +15,13 @@ class User:
 
     @staticmethod
     def from_dict(data):
-        return User(
+        return Users(
             username=data.get('username'),
             name=data.get('name'),
             password=data.get('password')
         )
 
-class Student:
+class Students:
     def __init__(self, studentID, name, creditsEarned):
         self.studentID = studentID
         self.name = name
@@ -36,8 +36,50 @@ class Student:
 
     @staticmethod
     def from_dict(data):
-        return Student(
+        return Students(
             studentID=data.get('studentID'),
             name=data.get('name'),
             creditsEarned=data.get('creditsEarned')
+        )
+        
+class Instructors:
+    def __init__(self, instructorID, name, department):
+        self.instructorID = instructorID
+        self.name = name
+        self.department = department
+
+    def to_dict(self):
+        return {
+            'instructorID': self.instructorID,
+            'name': self.name,
+            'department': self.department
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Instructors(
+            instructorID=data.get('instructorID'),
+            name=data.get('name'),
+            department=data.get('department')
+        )
+        
+class Courses:
+    def __init__(self, courseID, courseTitle, instructorID):
+        self.courseID = courseID
+        self.courseTitle = courseTitle
+        self.instructorID = instructorID
+
+    def to_dict(self):
+        return {
+            'courseID': self.courseID,
+            'courseTitle': self.courseTitle,
+            'instructorID': self.instructorID
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Courses(
+            courseID=data.get('courseID'),
+            courseTitle=data.get('courseTitle'),
+            instructorID=data.get('instructorID')
         )
